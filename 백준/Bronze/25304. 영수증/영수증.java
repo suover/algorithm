978d0e2e1d6f76aa4d5bd73163fe9a15ac576e99
Minsu Kim <151433475+suover@users.noreply.github.com>
2024-10-26 03:54:24 +0900

    [Bronze IV] Title: 영수증, Time: 108 ms, Memory: 13072 KB -BaekjoonHub

---
+++ 백준/Bronze/25304. 영수증/영수증.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int X = scanner.nextInt();
+		int N = scanner.nextInt();
+
+		int total = 0;
+
+		for (int i = 0; i < N; i++) {
+			int a = scanner.nextInt();
+			int b = scanner.nextInt();
+
+			total += a * b;
+		}
+
+		if (total == X) {
+			System.out.println("Yes");
+		} else {
+			System.out.println("No");
+		}
+ 	}
+}
