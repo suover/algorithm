a79a78f7dff357b56589b7e5bcdae29ee6e56533
Minsu Kim <151433475+suover@users.noreply.github.com>
2024-11-22 08:50:34 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 192 ms, Memory: 14332 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N-i; j++) {
+				System.out.print(" ");
+			}
+			for (int K = 1; K <= i; K++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
