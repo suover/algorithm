5156438e095f7a93718e54e1378a99af6110fce1
Minsu Kim <gts2044@gmail.com>
2024-12-11 18:26:53 +0900

    [Bronze V] Title: 개수 세기, Time: 100 ms, Memory: 12976 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int[] arr = new int[N];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = scanner.nextInt();
+		}
+		int v = scanner.nextInt();
+		int count = 0;
+
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == v) {
+				count++;
+			}
+		}
+		System.out.println(count);
+	}
+}
