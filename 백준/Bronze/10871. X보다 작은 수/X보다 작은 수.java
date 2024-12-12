f3b49a4c6aeb9cef04d087d7bff2a0e3293256b6
Minsu Kim <gts2044@gmail.com>
2024-12-12 22:27:33 +0900

    [Bronze V] Title: X보다 작은 수, Time: 384 ms, Memory: 26352 KB -BaekjoonHub

---
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int X = scanner.nextInt();
+
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = scanner.nextInt();
+			if (arr[i] < X) {
+				System.out.print(arr[i] + " ");
+			}
+		}
+	}
+}
