e46013405a5badd7be67f08a8f9094caf714f556
Minsu Kim <gts2044@gmail.com>
2024-12-13 15:51:54 +0900

    [Bronze III] Title: 최소, 최대, Time: 1776 ms, Memory: 314312 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int N = scanner.nextInt();
+		int[] arr = new int[N];
+
+		for (int i = 0; i < N; i++) {
+			arr[i] = scanner.nextInt();
+		}
+
+		Arrays.sort(arr);
+		System.out.println(arr[0] + " " + arr[N-1]);
+	}
+}
