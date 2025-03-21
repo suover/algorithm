be59baf5c11a81e1c9a3e2f1e294b523941daaf0
Minsu Kim <gts2044@gmail.com>
2025-03-21 17:33:10 +0900

    [Bronze IV] Title: 시험 점수, Time: 92 ms, Memory: 12844 KB -BaekjoonHub

---
+++ 백준/Bronze/5596. 시험 점수/시험 점수.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int S = 0;
+		int T = 0;
+
+		for (int i = 0; i < 4; i++) {
+			S += scanner.nextInt();
+		}
+		for (int i = 0; i < 4; i++) {
+			T += scanner.nextInt();
+		}
+
+		System.out.println(Math.max(S, T));
+	}
+}
