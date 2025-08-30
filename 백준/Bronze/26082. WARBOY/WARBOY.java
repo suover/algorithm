daad8bac308d188962ef06c7dab8663dbf059f63
Minsu Kim <gts2044@gmail.com>
2025-08-30 12:53:25 +0900

    [Bronze V] Title: WARBOY, Time: 92 ms, Memory: 12872 KB -BaekjoonHub

---
+++ 백준/Bronze/26082. WARBOY/WARBOY.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		int C = scanner.nextInt();
+		System.out.println(C * (B / A * 3));
+	}
+}
