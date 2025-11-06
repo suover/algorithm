d81d0e16a541705c11a966bde74ff7424fb04578
Minsu Kim <gts2044@gmail.com>
2025-11-06 20:08:27 +0900

    [Bronze V] Title: Cupcake Party, Time: 88 ms, Memory: 12820 KB -BaekjoonHub

---
+++ 백준/Bronze/24568. Cupcake Party/Cupcake Party.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r = sc.nextInt();
+		int s = sc.nextInt();
+
+		System.out.println(8 * r + 3 * s - 28);
+	}
+}
