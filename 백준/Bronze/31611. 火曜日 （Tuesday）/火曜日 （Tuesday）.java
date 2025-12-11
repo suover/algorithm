d9a89b179ee2533dd623f3923d30f52ac3894dc9
Minsu Kim <gts2044@gmail.com>
2025-12-11 21:42:01 +0900

    [Bronze V] Title: 火曜日 (Tuesday), Time: 92 ms, Memory: 12964 KB -BaekjoonHub

---
+++ 백준/Bronze/31611. 火曜日 （Tuesday）/火曜日 （Tuesday）.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+
+		if (x % 7 == 2) {
+			System.out.println(1);
+		} else {
+			System.out.println(0);
+		}
+	}
+}
