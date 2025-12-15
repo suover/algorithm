26b3cb4fec91074da431c8b1bd672dfcdc9929ad
Minsu Kim <gts2044@gmail.com>
2025-12-15 23:05:46 +0900

    [Bronze V] Title: 分 (Minutes), Time: 96 ms, Memory: 12868 KB -BaekjoonHub

---
+++ 백준/Bronze/31614. 分 （Minutes）/分 （Minutes）.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		System.out.println(h * 60 + m);
+	}
+}
